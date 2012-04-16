cbd41da5fef29ae61b21833a212a56cb6054d216
rw9uao@gmail.com <rw9uao@gmail.com@a5f4f3cd-c980-b933-9ba1-57208ebbe47f>
2012-04-16 15:29:17 +0000

    сохранение конфигурации на УСБ флэшку

--- dialogService.cpp
+++ dialogService.cpp
@@ -8,6 +8,8 @@ DialogService::DialogService(data_struct * d, QWidget *parent) : QDialog(parent)
     dui->setupUi(this);
     this->data = d;
 
+    dui->progressBar->hide();
+
     if(this->data->servicemode){
         dui->label->setText("Сервисный режим включен");
         dui->pushButton->show();
@@ -57,4 +59,75 @@ void DialogService::on_pushButton_clicked(){
 
 void DialogService::on_pushButton_2_clicked(){
     this->data->need_to_save_config = true;
+}
+
+void DialogService::on_save_clicked(){
+
+
+    //попробуем открыть /sys/block/sda
+    if (QDir("/sys/block/sda").exists() ){
+//    QFile config_file("/sys/block/sda/stat");
+//    if( config_file.open(QIODevice::ReadOnly) ){ // откроем файл для чтения
+//        config_file.close();
+        //попробуем смонтировать
+        qDebug("mount /dev/sda1 /mnt");
+        QStringList arguments; QProcess myProcess; QString program;
+        program = "mount";
+        arguments << "/dev/sda1" << "/mnt";
+        myProcess.start(program, arguments);
+        myProcess.waitForFinished();
+
+        //создадим папку с номером КНС
+        qDebug("mkdir /mnt/KHC_num");
+        QDir().mkdir(QString("/mnt/kns_%1").arg(this->data->KNSnumber));
+        qDebug("mkdir /mnt/KHC_num/bd");
+        QDir().mkdir(QString("/mnt/kns_%1/bd").arg(this->data->KNSnumber));
+
+        dui->progressBar->show();
+        //скопируем файлы
+        QStringList files = QDir("/nand/qt/").entryList(QStringList() << "*", QDir::Files);
+        dui->progressBar->setMaximum(files.size());
+        for(int i=0;i<files.size();i++){ // перебираем все эти файлы
+            dui->progressBar->setValue(i);
+            QStringList arguments; QProcess myProcess; QString program;
+            program = "cp";
+            arguments << "/nand/qt/"+files[i] << "/mnt/kns_" + this->data->KNSnumber + "/" + files[i];
+            qDebug() << "cp" << arguments[0] << arguments[1];
+            myProcess.start(program, arguments);
+            myProcess.waitForFinished();
+        }
+        QStringList files2 = QDir("/nand/qt/bd").entryList(QStringList() << "*", QDir::Files);
+        dui->progressBar->setMaximum(files2.size());
+        for(int i=0;i<files2.size();i++){ // перебираем все эти файлы
+            dui->progressBar->setValue(i);
+            QStringList arguments; QProcess myProcess; QString program;
+            program = "cp";
+            arguments << "/nand/qt/bd/"+files2[i] << "/mnt/kns_" + this->data->KNSnumber + "/bd/" + files2[i];
+            qDebug() << "cp" << arguments[0] << arguments[1];
+            myProcess.start(program, arguments);
+            myProcess.waitForFinished();
+        }
+
+
+        //отмонтируем флэшку
+        qDebug("umount /mnt");{
+        QStringList arguments; QProcess myProcess; QString program;
+        program = "umount";
+        arguments << "/mnt";
+        myProcess.start(program, arguments);
+        myProcess.waitForFinished();}
+
+        dui->progressBar->hide();
+
+        QMessageBox msgBox;
+        msgBox.setText("USB flash\nможно вынимать");
+        msgBox.setWindowTitle("Сохранение");
+        msgBox.exec();
+    }else{
+        QMessageBox msgBox;
+        msgBox.setText("USB flash not found");
+        msgBox.setWindowTitle("Сохранение");
+        msgBox.exec();
+        qDebug("/dev/sda not found");
+    }
 }
