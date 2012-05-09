8727b14739b3db64ed4cd44068f13952cfb92c8a
rw9uao@gmail.com <rw9uao@gmail.com@a5f4f3cd-c980-b933-9ba1-57208ebbe47f>
2012-05-09 07:54:28 +0000

    косметика. снова не все ушло. подная версия как всегда в архиве.

--- trunk/dialogNasosParamerer.cpp
+++ trunk/dialogNasosParamerer.cpp
@@ -6,6 +6,15 @@
 DialogNasosParameter::DialogNasosParameter(data_struct * d, QWidget *parent) : QDialog(parent), dui(new Ui::DialogNasosParameter){
     dui->setupUi(this);
     this->data = d;
+    this->setStyleSheet("QDialog{background-image: url(:/pict/pict/black.gif);}");
+    dui->label->setStyleSheet("color:rgb(255,255,255);");    dui->label_2->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_3->setStyleSheet("color:rgb(255,255,255);");    dui->label_4->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_5->setStyleSheet("color:rgb(255,255,255);");    dui->label_6->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_7->setStyleSheet("color:rgb(255,255,255);");    dui->label_8->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_9->setStyleSheet("color:rgb(255,255,255);");    dui->label_10->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_11->setStyleSheet("color:rgb(255,255,255);");    dui->label_12->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_13->setStyleSheet("color:rgb(255,255,255);");    dui->label_14->setStyleSheet("color:rgb(255,255,255);");
+    dui->label_15->setStyleSheet("color:rgb(255,255,255);");
     //если номер насоса не указан, завершим
     if(this->data->dialogparam < 0 || this->data->dialogparam > 3) return;
     dui->label_8->setText(QString("Насос %1").arg(this->data->dialogparam + 1));//номер запрошенного насоса
@@ -24,6 +33,12 @@ DialogNasosParameter::DialogNasosParameter(data_struct * d, QWidget *parent) : Q
     dui->label->setPixmap( QPixmap(QString("./bd/%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].pictname) ));
     //и артикул
     dui->label_9->setText(QString("арт %1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].articul));
+    dui->label_10->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].pressure));
+    dui->label_11->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].volume));
+    dui->label_12->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].current));
+    dui->label_13->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].rpm));
+    dui->label_14->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].cable));
+    dui->label_15->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].starttype));
 }
 
 DialogNasosParameter::~DialogNasosParameter(){
@@ -36,5 +51,12 @@ void DialogNasosParameter::on_comboBox_currentIndexChanged(int index){
 //        qDebug() << QString("index changed %1").arg(index);
         dui->label->setPixmap( QPixmap(QString("./bd/%1").arg(this->data->nasos_param[index].pictname) ));
         this->data->nasos_param_index[this->data->dialogparam] = index;
+        dui->label_9->setText(QString("арт %1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].articul));
+        dui->label_10->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].pressure));
+        dui->label_11->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].volume));
+        dui->label_12->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].current));
+        dui->label_13->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].rpm));
+        dui->label_14->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].cable));
+        dui->label_15->setText(QString("%1").arg(this->data->nasos_param[this->data->nasos_param_index[this->data->dialogparam]].starttype));
     }
 }
